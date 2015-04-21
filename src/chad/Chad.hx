95b4ec7f2e0106abdbe24a1eeee58621b3e47a77
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-20 22:44:41 -0600

    basic Chad infrastructure

---
+++ src/chad/Chad.hx
@@ -0,0 +1,37 @@
+package chad;
+
+import edge.World;
+import js.html.svg.SVGElement;
+import chad.systems.*;
+import thx.geom.d2.*;
+
+class Chad {
+  var world : World;
+  var svg : SVGElement;
+
+  public function new(svg : SVGElement) {
+    this.svg = svg;
+    world = new World(15);
+    addSystems();
+    // TODO remove
+    var p1 = Point.create(60, 60),
+        p2 = Point.create(90, 80);
+    world.engine.create([
+        Circle.fromPoints(p1, p2)
+      ]);
+    var incr = 1;
+    thx.core.Timer.repeat(function() {
+      p1.x += incr;
+      p2.y += incr;
+      if(p1.x > 300)
+        incr = -1;
+      else if(p1.x < 40)
+        incr = 1;
+    }, 10);
+    world.start();
+  }
+
+  public function addSystems() {
+    world.render.add(new RenderCircle(svg));
+  }
+}
