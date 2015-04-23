d0164338a3a2fb2f6096d8d530819f39286e72e2
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-22 23:30:25 -0600

    added selectedLayer to Chad

--- src/chad/Chad.hx
+++ src/chad/Chad.hx
@@ -12,21 +12,25 @@ class Chad {
   var world : World;
   var svg : SVGElement;
   var layers : Array<Tuple2<String, Layer>>;
+  var selectedLayer : Layer;
 
   public function new(svg : SVGElement) {
     this.svg = svg;
     this.layers = [];
+    this.selectedLayer = Layer.createFromSvg(svg);
     world = new World(20);
     addSystems();
     // TODO remove
     var layer = addLayer("my layer");
     var p1 = Point.create(60, 60),
-        p2 = Point.create(90, 80);
+        p2 = Point.create(180, 80);
     world.engine.create([
         Circle.fromPoints(p1, p2),
         LineStyle.constructionLine,
-        layer
+        layer,
+        Selected.instance
       ]);
+    /*
     var incr = 1;
     thx.Timer.repeat(function() {
       p1.x += incr;
@@ -36,6 +40,7 @@ class Chad {
       else if(p1.x < 40)
         incr = 1;
     }, 10);
+    */
     // END REMOVE
 
     world.start();
@@ -45,14 +50,19 @@ class Chad {
     if(null != getLayer(name))
       throw 'layer "$name" already exists';
     var layer = Layer.createFromSvg(svg);
+    // ensure it is always last
+    svg.appendChild(selectedLayer.group);
     layers.push(new Tuple2(name, layer));
     return layer;
   }
 
-  public function getLayer(name : String)
-    return layers.find(function(t) return t.left == name).right;
+  public function getLayer(name : String) {
+    var t = layers.find(function(t) return t.left == name);
+    return null == t ? null : t.right;
+  }
 
   public function addSystems() {
     world.render.add(new RenderCircle());
+    world.render.add(new RenderSelected(selectedLayer));
   }
 }
