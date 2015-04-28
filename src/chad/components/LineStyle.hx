a5643a51d231e5e2d08808b86bf53f0ca2598ab4
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-27 21:29:07 -0600

    added solid stroke color style

--- src/chad/components/LineStyle.hx
+++ src/chad/components/LineStyle.hx
@@ -1,7 +1,11 @@
 package chad.components;
 
+import thx.color.RGB;
+
 class LineStyle implements edge.IComponent {
   public static var constructionLine = new LineStyle(ConstructionLine);
+  public static function solidStroke(color : RGB, ?width : Float, ?alpha : Float)
+    return new LineStyle(SolidStroke(color, width, alpha));
 
   public static function apply(style : Style, node) switch style {
     case ConstructionLine:
@@ -9,6 +13,13 @@ class LineStyle implements edge.IComponent {
       node.setAttribute("stroke-width", "1");
       node.setAttribute("stroke", "#666666");
       node.setAttribute("stroke-dasharray", "5, 3");
+    case SolidStroke(color, width, alpha):
+      node.setAttribute("fill", "none");
+      node.setAttribute("stroke", color.toHex());
+      if(null != width)
+        node.setAttribute("stroke-width", '$width');
+      if(null != alpha)
+        node.setAttribute("stroke-opacity", '$alpha');
     case Selected:
       node.setAttribute("fill", "none");
       node.setAttribute("stroke-width", "2");
--- src/chad/components/Style.hx
+++ src/chad/components/Style.hx
@@ -1,6 +1,9 @@
 package chad.components;
 
+import thx.color.RGB;
+
 enum Style {
   ConstructionLine;
+  SolidStroke(color : RGB, ?width : Float, ?alpha : Float);
   Selected;
 }
